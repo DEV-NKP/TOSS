b537c080ab65d03b40ef26d12a19bbf1635218d7
DEV-NKP <niloykanti.paul2017@gmail.com>
2023-05-20 23:22:07 +0600

    Final V 1.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { TossModule } from './toss.module';
-
+import * as session from 'express-session';
+import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(TossModule);
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 3000000
+       }
+    }),
+  );
+  app.use(cookieParser());
 
+  app.use(cors({
+    origin: 'http://localhost:8000',
+    credentials: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
